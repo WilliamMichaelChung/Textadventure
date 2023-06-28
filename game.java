89d1bd30e8edf11070c94f6bc8daef772d4be0f3
chungwi <chungwi@whs.school.nz>
2023-06-29 11:56:31 +1200

    29/6/23 extra changes

--- game.java
+++ game.java
@@ -8,17 +8,18 @@
 import java.util.Scanner;//imports a scanner so the program can read what the user types
 public class game
 {
-    boolean item1;
-    boolean item2; 
-    boolean item3; 
-    boolean room1;
-    boolean room2;
-    boolean room3;
-    boolean room4;
-    boolean room5;
-    boolean room6;
-    boolean room7;
-    boolean room8;
+    boolean item1 = false;
+    boolean item2 = false; 
+    boolean item3 = false; 
+    boolean room1 = false;
+    boolean room2 = false;
+    boolean room3 = false;
+    boolean room4 = false;
+    boolean room5 = false;
+    boolean room6 = false;
+    boolean room7 = false;
+    boolean room8 = false;
+    boolean exit = false;
     int[] numberOfMoves = {15};//The amount of moves the player is given. If used up they lose
 
     /**
@@ -155,13 +156,52 @@ public class game
         }//if the user enter room3/if room 3 is true then print out a description of the room
         if(room3 && gameKeyboard.equals ("f")){
             System.out.println("There is nothing in here");
-        }// if the user is in room 3 and inputs f to search the room they'll find item 1 and make the item 1 boolean true
+        }// if the user is in room 3 and inputs f to search the room they'll find nothing
 
         //room 4 code
         if(room4){
             System.out.println("trap room. you fell to your death");
-            System.exit();
+            System.exit(0);
         }//if the user enter room4/if room 4 is true then print out a description of the room
+
+        //room 5 code
+        if(room5){
+            System.out.println("description of the room");
+        }//if the user enter room4/if room 4 is true then print out a description of the room
+        if(room5 && gameKeyboard.equals ("f")){
+            System.out.println("You found item 2");
+            item2 = true;
+        }// if the user is in room 5 and inputs f to search the room they'll find item 2 and make the item 2 boolean true
+
+        //room 6 code
+        if(room6){
+            System.out.println("trap room. you fell to your death");
+            System.exit(0);
+        }//if the user enter room6/if room 6 is true then print out a description of the room
+
+        //room 7 code
+        if(room7){
+            System.out.println("description of the room");
+        }//if the user enter room 7/if room 7 is true then print out a description of the room
+        if(room7 && gameKeyboard.equals ("f")){
+            System.out.println("You found item 3");
+            item3 = true;
+        }// if the user is in room 7 and inputs f to search the room they'll find item 3 and make the item 3 boolean true
+
+        //room 8
+        if(room8){
+            System.out.println("description of the room");
+        }//if the user enter room4/if room 4 is true then print out a description of the room
+        if(room8 && gameKeyboard.equals ("f")){
+            System.out.println("You found item 3");
+            item3 = true;
+        }// if the user is in room 8 and inputs f to search the room they'll stumble upon an exit
+
+        //exit room
+        if(exit && item1 && item2 && item3){
+            System.out.println("You win!");
+            System.exit(0);
+        }//if exit, item1, item2 and item 3 have been found/are true then the user wins and the program ends
 
         /**
         if(gameKeyboard.equals("e")){
